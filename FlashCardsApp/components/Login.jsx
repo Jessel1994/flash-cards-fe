9b324d26fc32317d269548401382e56b088e08eb
Sergii Golovko <golovko.sergey@gmail.com>
2023-11-17 12:09:06 +0000

    styling fixes

--- FlashCardsApp/components/Login.jsx
+++ FlashCardsApp/components/Login.jsx
@@ -1,41 +1,45 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import { UserContext, UserProvider } from '../contexts/Theme';
 import { getUsers } from '../api';
 
 const Login = ({ navigation }) => {
-  const {user, setUser} = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [profiles, setProfiles] = useState([])
-  const [loginError, setLoginError] = useState(null)
+  const [profiles, setProfiles] = useState([]);
+  const [loginError, setLoginError] = useState(null);
 
-
-  useEffect (() => {
+  useEffect(() => {
     getUsers().then((response) => {
-     
-      setProfiles(response)
-      
-    })
-  }, [])
- 
- 
+      setProfiles(response);
+    });
+  }, []);
 
   const handleLogIn = () => {
-    
-    const foundUser = profiles.find(profile => profile.username === username && profile.password === password);
-    
+    const foundUser = profiles.find(
+      (profile) =>
+        profile.username === username && profile.password === password
+    );
+
     if (foundUser) {
       setUser(foundUser); // Set the found user in context
-      
-      setUsername(""); // Clear the username state
-      setPassword(""); // Clear the password state
+
+      setUsername(''); // Clear the username state
+      setPassword(''); // Clear the password state
       navigation.navigate('Welcome'); // Navigate to the next screen
     } else {
-      setLoginError("Invalid username or password");
+      setLoginError('Invalid username or password');
     }
   };
-  
 
   const navigateToSignUp = () => {
     // Use navigation to navigate to the "SignUp" page
@@ -44,29 +48,40 @@ const Login = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Image
+        style={{
+          width: 300,
+          height: 200,
+        }}
+        source={require('../img/adaptive-icon.png')}
+      />
       <View style={styles.inputContainer}>
         <Text>Username</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter your username"
+          placeholder='Enter your username'
           value={username}
-          onChangeText={(text) => {setUsername(text)}}
+          onChangeText={(text) => {
+            setUsername(text);
+          }}
         />
       </View>
 
       <View style={styles.inputContainer}>
         <Text>Password</Text>
-        
+
         <TextInput
           style={styles.input}
-          placeholder="Enter your password"
+          placeholder='Enter your password'
           secureTextEntry
           value={password}
-          onChangeText={(text) => {setPassword(text)}}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
         />
       </View>
       {loginError && <Text>{loginError}</Text>}
-      
+
       <TouchableOpacity style={styles.button} onPress={handleLogIn}>
         <Text style={styles.buttonText}>Log In</Text>
       </TouchableOpacity>
@@ -75,7 +90,6 @@ const Login = ({ navigation }) => {
         <Text style={styles.signupText}>Haven't got an account? Sign Up</Text>
       </TouchableOpacity>
       {loginError && <Text style={styles.errorText}>{loginError}</Text>}
-
     </View>
   );
 };
@@ -102,7 +116,8 @@ const styles = StyleSheet.create({
     fontSize: 16, // Increase the font size
     backgroundColor: '#f7f7f7', // A light gray background color for the input field
   },
-  button: { // This style is for the login button
+  button: {
+    // This style is for the login button
     width: '100%',
     height: 50, // Match the height of the text inputs
     backgroundColor: '#34C759', // The green color from the image
@@ -111,7 +126,8 @@ const styles = StyleSheet.create({
     borderRadius: 25, // Round the corners
     marginTop: 10, // Add some margin at the top
   },
-  buttonText: { // This style is for the text inside the button
+  buttonText: {
+    // This style is for the text inside the button
     color: '#fff', // White text color
     fontSize: 18, // Increase the font size
     fontWeight: 'bold', // Make the text bold
@@ -130,6 +146,5 @@ const styles = StyleSheet.create({
     marginBottom: 20, // Space between error text and input fields
   },
 });
-
 
 export default Login;
